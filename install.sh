fef904ba5700869ca179f890d5630f7d6facbd1e
Prajjwal Verma <prajjwalverma74@gmail.com>
2026-01-06 01:21:50 +0530

    fix: cp was copying one directory deeper

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo pacman -S matugen ghostty neovim swaync waybar rofi dolphin hyprpolkitagent swww ttf-space-mono-nerd
+sudo pacman -S matugen ghostty neovim swaync waybar rofi dolphin hyprpolkitagent swww ttf-space-mono-nerd wl-clipboard
 hyprpm update
 hyprpm add https://github.com/hyprwm/hyprland-plugins
 hyprpm enable hyprscrolling
 cp -r Wallpapers ~/Pictures/Wallpapers
-cp -r rofi ~/.config/rofi
-cp -r ghostty ~/.config/ghostty
-cp -r swaync ~/.config/swaync
-cp -r nvim ~/.config/nvim
-cp -r waybar ~/.config/waybar
-cp -r matugen ~/.config/matugen
-cp -r hypr ~/.config/hypr
+cp -r rofi ~/.config
+cp -r ghostty ~/.config
+cp -r swaync ~/.config
+cp -r nvim ~/.config
+cp -r waybar ~/.config
+cp -r matugen ~/.config
+cp -r hypr ~/.config
