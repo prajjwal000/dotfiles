a521c962a723d23e49b7280030e99dbe10c8acd7
Prajjwal Verma <prajjwalverma74@gmail.com>
2026-01-06 03:22:40 +0530

    fix: remove harcoded user

--- scripts/wall-switch.sh
+++ scripts/wall-switch.sh
@@ -6,7 +6,7 @@ cd "$WALL_DIR"
 
 IFS=$'\n'
 
-SELECTED_WALL=$(for a in *.jpg *.png; do echo -en "$a\0icon\x1f$a\n" ; done | rofi -dmenu -p "🎨 Select Wallpaper" -show-icons -theme "/home/treeplanter/.local/scripts/solarized-osaka.rasi")
+SELECTED_WALL=$(for a in *.jpg *.png; do echo -en "$a\0icon\x1f$a\n" ; done | rofi -dmenu -p "🎨 Select Wallpaper" -show-icons -theme "~/.local/scripts/solarized-osaka.rasi")
 
 if [ -n "$SELECTED_WALL" ]; then
     matugen image "$SELECTED_WALL"
